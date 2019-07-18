df7d34751c95ee8f83a579a77614e79abd82e68c
dwief <dwieferich@usgs.gov>
2019-07-18 11:58:17 -0600

    first commit draft lk watershed process

---
+++ README.md
@@ -0,0 +1,44 @@
+*** In Progress *** Global Freshwater Fisheries Assessment
+===============================================================================================================
+
+-----------
+Contact:
+-----------
+Daniel Wieferich: dwieferich@usgs.gov
+
+
+-----------
+Collaborators:
+-----------
+Gretchen Stokes: gstokes@usgs.gov
+Abby Lynch: ajlynch@usgs.gov 	
+Simon Funge-Smith: Simon.FungeSmith@fao.org
+Food and Agriculture Organization of the United Nations		  
+	
+
+
+
+-----------
+Purpose:
+-----------
+Document processes and methods to help support an ongoing Global Freshwater Fisheries Assessment.
+
+-----------
+Additional Details:
+-----------
+This will be filled in as the project progresses. 
+
+
+-----------
+Development Status:
+-------------------
+Software documented in this repository are unpublished and will be under continual development.  Collaborative efforts to help improve code and analyses are welcomed.
+
+
+
+
+
+
+# USGS Provisional Software
+
+This software is preliminary or provisional and is subject to revision. It is being provided to meet the need for timely best science. The software has not received final approval by the U.S. Geological Survey (USGS). No warranty, expressed or implied, is made by the USGS or the U.S. Government as to the functionality of the software and related material nor shall the fact of release constitute any such warranty. The software is provided on the condition that neither the USGS nor the U.S. Government shall be held liable for any damages resulting from the authorized or unauthorized use of the software.
---
+++ build_lk_catchment_mp.py
@@ -0,0 +1,55 @@
+
+from utils import build_lk_catchment as build_lk
+import geopandas as gpd
+from multiprocessing import Pool
+from functools import partial
+import os
+
+def run_lake(lake):
+    if hasattr(lake, 'dir_file'):
+        flow_dir_file = lake.dir_file
+        print (f"processing: {lake}" )
+        build_lk.watershed(flow_dir_file,lake,55)
+
+if __name__ == '__main__':
+
+
+    lake_file = 'data/HydroLAKES_polys_v10.gdb'
+    lakes_df = gpd.read_file(lake_file, layer= 'HydroLAKES_polys_v10')
+
+    #Create list of objects for each lake
+    lakes_info = []
+    for lake in lakes_df.itertuples():
+        lake_id = lake.Hylak_id
+        continent = lake.Continent
+        wshd_area = lake.Wshd_area
+        pour_pt_lat = lake.Pour_lat
+        pour_pt_lon = lake.Pour_long
+        geom = lake.geometry
+        lk = build_lk.Lake(lake_id, continent, wshd_area, pour_pt_lat, pour_pt_lon, geom)
+        lk.which_dir_raster()
+        lakes_info.append(lk)
+    
+
+
+    dirName = "output/lkshed"
+    # Create target directory & all intermediate directories if don't exists
+    try:
+        os.makedirs(dirName)    
+        print("Directory " , dirName ,  " Created ")
+    except FileExistsError:
+        print("Directory " , dirName ,  " already exists") 
+
+    for lake in lakes_info:
+        if hasattr(lake, 'dir_file'):
+            flow_dir_file = lake.dir_file
+            print (f"processing: {lake}" )
+            build_lk.watershed(flow_dir_file,lake,55)
+
+    p = Pool(6)
+    list_of_results = pool.map(run_lake, ((lake for lake in lakes_info)))
+    p.close()
+    p.join()
+
+
+        
